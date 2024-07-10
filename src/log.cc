5cd07c340e071365550a5577b36b001d8635887a
shale <slucas001@citymail.cuny.edu>
2024-07-09 23:13:20 -0400

    added the to_string function to the logger

--- src/log.cc
+++ src/log.cc
@@ -1,7 +1,30 @@
-#include <iostream>
+#include "../include/log.hh"
+#include <source_location>
 
-int main (void) {
+timber::log::log (const config& conf) {
 	
-	std::cout << "hello, world" << std::endl;
-	return 0; 
+	// Do something with inital state of logger
+	auto config_file = toml::parse_file(conf.file_path);
+	for (auto&& [key, value] : config_file) {
+	
+	}	
+	
 }
+                                                                               
+
+std::string timber::log::to_string (const log& logger) const {
+	level current = logger.current_level;
+
+	switch (current) {
+		case level::WARN:  
+			return std::string("[warning]");
+		case level::DEBUG:  
+			return std::string("[debug]");
+		case level::INFO:  
+			return std::string("[info]");
+		case level::FATAL:  
+			return std::string("[fatal]");
+		
+	} 
+}
+
