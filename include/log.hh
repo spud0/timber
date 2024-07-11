8aa384d4f5607f16bedfc5aae6ec259e6ef5080f
shale <slucas001@citymail.cuny.edu>
2024-07-10 22:41:05 -0400

    added inline map for colors

--- include/log.hh
+++ include/log.hh
@@ -25,15 +25,20 @@ namespace timber {
 		
 	  private:
 		std::filesystem::path file_path;
-		std::unordered_map<std::string, color> level_color_map;
+		inline static const std::unordered_map<std::string, color> level_color_map = {
+			{"\u001b[40m", color::BLACK }, {"\u001b[41m", color::RED},
+			{"\u001b[42m", color::GREEN} , {"\u001b[43m", color::YELLOW},
+			{"\u001b[44m", color::BLUE}  , {"\u001b[45m", color::MAGENTA},
+			{"\u001b[46m", color::CYAN}  , {"\u001b[47m", color::WHITE}, 
+		};
 		toml::table config_table;	
 	};
 
 	// may have too much responsibility
 	class log {	
 	  public:
-		[[nodiscard]] constexpr std::string_view to_string (const log& logger) const noexcept;
-		[[nodiscard]] std::string log_message (std::string_view message, std::source_location loc) const;
+		[[nodiscard]] std::string_view to_string (const log& logger) const noexcept;
+		[[nodiscard]] std::string log_message (std::string_view message, timber::level current, std::source_location loc) const;
 		
 		log (const config& conf);
 		~log() = default;
