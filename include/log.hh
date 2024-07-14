0f94db5289e1a3417be173e6dad79ddd7dbbfd0b
shale <slucas001@citymail.cuny.edu>
2024-07-13 23:43:04 -0400

    some new functions and shit

--- include/log.hh
+++ include/log.hh
@@ -7,6 +7,7 @@
 #include <filesystem>
 #include <expected>
 #include <unordered_map>
+#include <fmt/color.h>
 
 // For TOML parsing
 #include "./ext/toml.hh"
@@ -14,31 +15,50 @@
 namespace timber {
 	
 	enum class level { WARN, DEBUG, INFO, FATAL, };
-	enum class loc   { STDOUT, NET, DISK, };
-	enum class color { BLACK, RED, GREEN, YELLOW, BLUE, MAGENTA, CYAN, WHITE, };
+	enum class destn { STDOUT, NET, DISK, };
 
 	// The config for the logger
 	class config {
 	  public:
-		static std::expected<config, std::system_error> read_from_toml(const std::filesystem::path& file_path);
-		[[nodiscard]] const toml::table& get_config_table(void) const noexcept;
+		static std::expected<config, std::system_error> read_from_toml(const std::filesystem::path& file_path) noexcept;
+
+		[[nodiscard("Must capture TOML table to configure logger.")]] const toml::table& get_config_table(void) const noexcept;
+
+		[[nodiscard]] const std::string_view get_color (level lvl) const noexcept; 
+
+		[[nodiscard]] const config build_from_table(const toml::table& table) const noexcept;
+		
 		
 	  private:
+
 		std::filesystem::path file_path;
-		inline static const std::unordered_map<std::string, color> level_color_map = {
-			{"\u001b[40m", color::BLACK }, {"\u001b[41m", color::RED},
-			{"\u001b[42m", color::GREEN} , {"\u001b[43m", color::YELLOW},
-			{"\u001b[44m", color::BLUE}  , {"\u001b[45m", color::MAGENTA},
-			{"\u001b[46m", color::CYAN}  , {"\u001b[47m", color::WHITE}, 
-		};
 		toml::table config_table;	
+
+		inline static const std::unordered_map<std::string, fmt::color> level_color_map = {
+			{"black", fmt::color::black }, {"red",     fmt::color::red},
+			{"green", fmt::color::green} , {"yellow",  fmt::color::yellow},
+			{"blue",  fmt::color::blue}  , {"magenta", fmt::color::magenta},
+			{"cyan",  fmt::color::cyan}  , {"white",   fmt::color::white}, 
+		};
+
+		// Some reasonable default settings
+		std::string_view destn = "stdout";		
+		bool defaults_enabled = false;
+		bool buffer_enabled = false; 
+
+		size_t buffer_size = 15;
+			
+		[[noreturn]] void parse_toml_table() const noexcept; 
+			
 	};
 
-	// may have too much responsibility
+	// Does the logging with log_message
 	class log {	
 	  public:
 		[[nodiscard]] std::string_view to_string (const log& logger) const noexcept;
-		[[nodiscard]] std::string log_message (std::string_view message, timber::level current, std::source_location loc) const;
+		[[noreturn]]  void log_message (timber::level current,
+										std::string_view message, 
+										const std::source_location loc = std::source_location::current()) const;
 		
 		log (const config& conf);
 		~log() = default;
