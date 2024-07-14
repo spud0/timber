5a00136103a10608fe41c268eb2d16ad7b32bf33
shale <slucas001@citymail.cuny.edu>
2024-07-13 23:39:33 -0400

    added bunch of if statements

--- src/log.cc
+++ src/log.cc
@@ -2,51 +2,76 @@
 #include <expected>
 #include <system_error>
 #include <mutex>
+#include <fmt/core.h>
 
 #include "../include/log.hh"
 
-[[nodiscard]] const toml::table timber::config::get_config_table(void) const noexcept {
-	
-}
+namespace timber {
 
-// Use because opening the file can cause a problem
-std::expected<config, std::system_error> timber::config::read_from_toml (const std::filesystem::path& path) {
-	
-}
+	[[nodiscard]] 
+	const toml::table& timber::config::get_config_table(void) const noexcept {
+		return config_table;	
+	}
 
-timber::log::log (const config& conf) {
-	
-	// Do something with inital state of logger
-	auto config_table = conf.get_config_table;
-	for (auto&& [key, value] : config_file) {
+	// Use because opening the file can cause a problem
+	std::expected<toml::table, std::string> config::read_from_toml (const std::filesystem::path& path) noexcept {
 		
-	}	
+		if (path.string().empty()) {
+			return std::unexpected("No filepath provided");
+		}
 	
-}
-                                                                               
+		// should this be in a try except??	 does throw I think
+		auto tbl= toml::parse_file(path.string());	
+		return tbl.table();
+	}
 
-[[nodiscard]]
-std::string_view constexpr timber::log::to_string (const log& logger) const noexcept {
 
-	level current = logger.current_level;
-	switch (current) {
-		case level::WARN:   return "[warning]";
-		case level::DEBUG:  return "[debug]";
-		case level::INFO:   return "[info]";
-		case level::FATAL:  return "[fatal]";
+	[[nodiscard]] 
+	const std::expected<config, std::string> config::build_from_table(toml::table table) const noexcept {
+	
+		if (!table.empty())	{
+							
+		}
+	}
+
+	log::log (const config& conf) {
 		
-	} 
-	
-	return ""; 
-}
+		// Do something with inital state of logger
+		auto config_table = conf.get_config_table();
+		for (auto&& [key, value] : config_table) {
+			
+		}	
+		
+	}
+																				   
+
+	[[nodiscard]]
+	std::string_view log::to_string (const log& logger) const noexcept {
+
+		level current = logger.current_level;
+		switch (current) {
+			case level::WARN:   return "[warning]";
+			case level::DEBUG:  return "[debug]";
+			case level::INFO:   return "[info]";
+			case level::FATAL:  return "[fatal]";	
+		} 
+		
+		return ""; 
+	}
 
 
-[[nodiscard]]
-std::expected<std::string, std::system_error> timber::log::log_message (std::string_view mess,
-						 std::source_location loc = std::source_location::current()
-) const {
+	[[noreturn]]
+	void log::log_message (
+			level current,
+			std::string_view mess,
+			const std::source_location loc = std::source_location::current()
+	) const {
 
-	std::lock_guard<std::mutex> lock (_mtx);
-	
-	return "";
+		std::lock_guard<std::mutex> lock (_mtx);
+		fmt::format_args();
+		
+		std::string_view local_ctx = loc.file_name() + " : " + loc.line() + " : " + loc.function_name();
+		
+	}
+
 }
