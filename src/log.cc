f40cfa79cd4fe5dc1f459274f5bc4fd20d060dd1
shale <slucas001@citymail.cuny.edu>
2024-07-15 23:13:18 -0400

    alot of shit here

--- src/log.cc
+++ src/log.cc
@@ -13,25 +13,68 @@ namespace timber {
 		return config_table;	
 	}
 
+	#if 0
 	// Use because opening the file can cause a problem
-	std::expected<toml::table, std::string> config::read_from_toml (const std::filesystem::path& path) noexcept {
+	toml::table config::read_from_toml (const std::filesystem::path& path) noexcept {
 		
 		if (path.string().empty()) {
 			return std::unexpected("No filepath provided");
 		}
 	
 		// should this be in a try except??	 does throw I think
-		auto tbl= toml::parse_file(path.string());	
-		return tbl.table();
+		auto tbl = toml::parse_file(path.string());	
+		return tbl;
 	}
+	#endif
 
-
-	[[nodiscard]] 
-	const std::expected<config, std::string> config::build_from_table(toml::table table) const noexcept {
+	[[nodiscard("Must capture config to configure logger.")]] 
+	const config config::build_from_table(const toml::table& table) const noexcept {
 	
-		if (!table.empty())	{
-							
+		config conf{};
+
+		// Return an empty congif if the table is empty	
+		if (table.empty())	{
+			return conf;			
 		}
+
+		if (table.contains("logger")) {
+			const auto& logger = config_table["logger"];
+
+			bool use_defaults = logger["defaults"].value_or(false).as_boolean();
+			if (use_defaults) {
+				// Do something for default configuration values?
+				// Defaults already baked into the class 
+			} else {
+				destn = logger["destination"].value_or("stdout").as_string();
+				format_string = logger["format"].value_or("{timestamp} [{level}] {message}").as_string();
+
+				if (logger.contains("buffer")) {
+					const auto& buffer = logger["buffer"];
+					buffer_enabled = buffer["enabled"].value_or(true).as_boolean();
+					buffer_size = buffer["buffer_size"].value_or(10).as_integer();
+				}
+
+				if (logger.contains("colors")) {
+					const auto& colors = logger["colors"];
+					debug_color = colors["debug"].value_or("blue").as_string();
+					info_color = colors["info"].value_or("green").as_string();
+					warn_color = colors["warn"].value_or("yellow").as_string();
+					error_color = colors["error"].value_or("red").as_string();
+				}
+
+				if (logger.contains("file")) {
+					const auto& file = logger["file"];
+					conf.set_fp(file["path"].as_string());
+					conf.set_rot_size(file["rotation_size"].value_or(10485760).as_integer());
+				}
+
+				if (logger.contains("stdout")) {
+					const auto& stdout_section = logger["stdout"];
+					stdout_enabled = stdout_section["enabled"].value_or(true).as_boolean();
+				}
+			}	
+
+		return conf;
 	}
 
 	log::log (const config& conf) {
